cdd282282ef0607b56c81e9a75d9c91abb459cb1
Romain Fairant <romain.fairant@orness.com>
2019-12-04 10:58:31 +0100

    clusterGandalf

--- cluster/ClusterGandalf.go
+++ cluster/ClusterGandalf.go
@@ -1,1 +1,16 @@
-package clustergandalf
+package cluster
+
+type ClusterGandalf struct {
+	clusterCommandRoutine ClusterCommandRoutine
+	clusterEventRoutine   ClusterEventRoutine
+}
+
+func (cg ClusterGandalf) main() {
+	//identity, workerCommandReceiveC2WConnection, workerEventReceiveC2WConnection string, topics *string
+	//LOAD CONF
+	cg.clusterCommandRoutine = ClusterCommandRoutine.new()
+	cg.clusterEventRoutine = ClusterEventRoutine.new()
+
+	go cg.clusterCommandRoutine.run()
+	go cg.clusterEventRoutine.run()
+}
