66aa50d359681482a50f384015d722a63c0e8b9e
t1briquet <thomas.briquet@isen.ynrea.fr>
2020-06-08 16:12:24 +0200

    configuration par flags

--- core.go
+++ core.go
@@ -274,12 +274,6 @@ func main() {
 		flag.Usage()
 	}  */
 
-
-	configuration.SetCoreFlags()
-	configuration.SetCoreConfig()
-	configuration.PrintCoreConfig()
-
-	configuration.SetConnectorFlags()
-	configuration.SetConnectorConfig()
-	configuration.PrintConnectorConfig()
+	configuration.SetConfiguration()
+	//configuration.PrintConfig()
 }
