fbf589fc0142788f542c17ecae8739b175209f88
Romain Fairant <romain.fairant@orness.com>
2020-10-29 14:25:52 +0100

    go

--- connectors/go/go.mod
+++ connectors/go/go.mod
@@ -2,4 +2,7 @@ module github.com/ditrit/gandalf/connectors/go
 
 go 1.13
 
-require github.com/ditrit/gandalf/libraries/goclient v0.0.0-20201026165739-6894d8c7142b
+require (
+	github.com/ditrit/gandalf/libraries/goclient v0.0.0-20201026165739-6894d8c7142b
+	github.com/ditrit/shoset v0.0.0-20201026092509-225b8a4a5276
+)
--- connectors/go/worker.go
+++ connectors/go/worker.go
@@ -1,9 +1,7 @@
 package worker
 
 import (
-	"encoding/json"
-	"gandalf/connectors/goutilscustom/mail"
-	"gandalf/libraries/goclient/models"
+	"github.com/ditrit/gandalf/libraries/goclient/models"
 
 	"github.com/ditrit/gandalf/connectors/go/functions"
 	goclient "github.com/ditrit/gandalf/libraries/goclient"
@@ -18,8 +16,8 @@ type Worker struct {
 	clientGandalf   *goclient.ClientGandalf
 	CommandesActive map[string]int
 	EventsActive    map[string]int
-	CommandesFuncs  map[string]func(clientGandalf *goclient.ClientGandalf, major, minor int64) int
-	EventsFuncs     map[string]func(clientGandalf *goclient.ClientGandalf, major, minor int64) int
+	CommandesFuncs  map[string]func(clientGandalf *goclient.ClientGandalf, major, minor int64, command msg.Command) int
+	EventsFuncs     map[string]func(clientGandalf *goclient.ClientGandalf, major, minor int64, event msg.Event) int
 	Start           func() *goclient.ClientGandalf
 	SendCommands    func(clientGandalf *goclient.ClientGandalf, major, minor int64, commandes []string)
 	//Execute      func()
@@ -79,20 +77,15 @@ func (w Worker) Run() {
 	<-done */
 }
 
-func (w Worker) waitCommands(id, commandName string, function func(clientGandalf *goclient.ClientGandalf, major, minor int64) int) {
+func (w Worker) waitCommands(id, commandName string, function func(clientGandalf *goclient.ClientGandalf, major, minor int64, command msg.Command) int) {
 	command := w.clientGandalf.WaitCommand(commandName, id, w.major)
+	w.CommandesActive[commandName]++
+	go w.executeCommands(command, function)
 
-	var mailPayload mail.MailPayload
-	err := json.Unmarshal([]byte(command.GetPayload()), &mailPayload)
-
-	if err != nil {
-		w.CommandesActive[commandName]++
-		go w.executeCommands(command, function)
-	}
 }
 
-func (w Worker) executeCommands(command msg.Command, function func(clientGandalf *goclient.ClientGandalf, major, minor int64) int) {
-	result := function(w.clientGandalf, w.major, w.minor)
+func (w Worker) executeCommands(command msg.Command, function func(clientGandalf *goclient.ClientGandalf, major, minor int64, command msg.Command) int) {
+	result := function(w.clientGandalf, w.major, w.minor, command)
 	if result == 0 {
 		w.clientGandalf.SendReply(command.GetCommand(), "SUCCES", command.GetUUID(), models.NewOptions("", ""))
 	} else {
@@ -102,7 +95,7 @@ func (w Worker) executeCommands(command msg.Command, function func(clientGandalf
 }
 
 //TODO REVOIR
-func (w Worker) WaitEvents(id, eventName string, function func(clientGandalf *goclient.ClientGandalf, major, minor int64) int) {
+func (w Worker) WaitEvents(id, eventName string, function func(clientGandalf *goclient.ClientGandalf, major, minor int64, event msg.Event) int) {
 	/* event := w.clientGandalf.WaitEvent(eventName, id, w.major)
 
 	var mailPayload mail.MailPayload
@@ -115,7 +108,7 @@ func (w Worker) WaitEvents(id, eventName string, function func(clientGandalf *go
 }
 
 //TODO REVOIR
-func (w Worker) ExecuteEvents(event msg.Event, function func(clientGandalf *goclient.ClientGandalf, major, minor int64) int) {
+func (w Worker) ExecuteEvents(event msg.Event, function func(clientGandalf *goclient.ClientGandalf, major, minor int64, event msg.Event) int) {
 	/* 	result := function(w.clientGandalf, w.major, w.minor)
 	   	if result == 0 {
 	   		//w.clientGandalf.SendReply(command.GetCommand(), "SUCCES", command.GetUUID(), models.NewOptions("", ""))
