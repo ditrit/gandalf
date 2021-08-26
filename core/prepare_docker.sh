9e941f4fce363c80c0746e077a28759159bf63cd
Xavier Talon <xavier.talon@orness.com>
2021-08-26 10:43:59 +0200

    generate Dockerfile

--- core/prepare_docker.sh
+++ core/prepare_docker.sh
@@ -20,6 +20,21 @@ cp -r certs $CONFDIR/
 cp -r certs $INSTDIR/
 chmod go-rwx $CONFDIR/certs/*
 tar czvf fs4docker.tgz -C $TMPDIR .
+cat > Dockerfile <<EOF
+FROM ubuntu:21.04
+# deploy gandalf filesystem 
+ADD fs4docker.tgz /
+# install dependencies
+RUN ln -fs /usr/share/zoneinfo/Europe/Paris /etc/localtime
+RUN apt-get update
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y apt-utils 
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y ca-certificates tzdata
+# prepare ebnvironment
+EXPOSE 9203
+WORKDIR /usr/local/bin
+# command to launch at the startup of the container 
+CMD rungandalf.sh
+EOF
 sudo docker rm `sudo docker ps -a | grep "geandalfdocker" | cut -d" " -f1 | grep -v CONTAINER`
 sudo docker rmi gandalfdocker
 sudo docker build -t gandalfdocker .
